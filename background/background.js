512a5228446ecf53c24ca458ef55706b3ce6a2f6
Sven Mesecke <sven.mesecke@sveme.org>
2016-06-20 08:30:36 +0200

    bug fixes and some changes to popup inputs

--- background/background.js
+++ background/background.js
@@ -1,33 +1,30 @@
 // needs to get a message from the popup script about which sites to block
-var blockList = new Map();
-var urlFilter = {"urls": []};
-const extraInfo = {"blocking": true};
+const blockList = new Map();
+const urlFilter = { urls: [] };
 
-function newBlock(block){
+function newBlock(block) {
   blockList.set(block.url, block.blockUntil);
   urlFilter.urls.append(block.url);
 }
-function checkBlock(details){
+function checkBlock(details) {
   let block = false;
-  let now = new Date();
-  let blockUntil = blockList.get(details.url); //TODO change so that it works with url matches
-  if (now > blockUntil){ //TODO does algebra work on date objects?
+  const now = new Date();
+  const blockUntil = blockList.get(details.url); // TODO change so that it works with url matches
+  if (now > blockUntil) { // TODO does algebra work on date objects?
     block = true;
-  }
-  else {
+  } else {
     block = false;
     blockList.delete(details.url); // remove block when block time is superseded
   }
-  if (block){
-    return {"blocking": true} //TODO
-  }
-  else {
-    return null //TODO
+  if (block) {
+    return { cancel: true }; // TODO
+  } else {
+    return null; // TODO
   }
 }
 // any web request needs to be filtered - add listener to webRequests
 // block a webRequest when url is in blockList
 // when timeNow is > blockedTime, remove block from list
 
-chrome.webRequest.onBeforeRequest.addListener(checkBlock, urlFilter, extraInfo);
+chrome.webRequest.onBeforeRequest.addListener(checkBlock, urlFilter, ['blocking']);
 chrome.runtime.onMessage.addListener(newBlock);
--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,34 @@
-function blockSite(e){
+const hoursSelected = document.getElementById("hour");
+const minutesSelected = document.getElementById("minutes");
+
+function blockSite(e) {
+	let hours = 0;
+	let minutes = 0;
+	try {
+		hours = hoursSelected.value;
+	}
+	catch(exception) {
+		hours = 0;
+	}
+	try {
+		minutes = minutesSelected.value;
+	}
+	catch (exception){
+		minutes = 0;
+	}
+	// sanitize inputs - make sure that it's numeric only TODO
 	console.log("works");
-	let hours = document.getElementById("hour").value;
-	let minutes = document.getElementById("minutes").value;
+	console.log(hours);
+	console.log(minutes);
+
 	let blockUntil = new Date();
 	if (hours > 0){
-		blockUntil.setHours(hours);
+		blockUntil.setHours(hours); // TODO get time arithmetics right
 	}
 	if (minutes > 0){
 		blockUntil.setMinutes(minutes);
 	}
 
-	console.log(hours);
-	console.log(minutes);
 	console.log(blockUntil);
 
 	chrome.tabs.getCurrent(
@@ -19,7 +36,7 @@ function blockSite(e){
 			let url = tab.url; // TODO allow url matching patterns
 	})
 	console.log(url);
-	let blockedSite = {"url": url, "blockUntil":blockUntil};
+	let blockedSite = {'url': url, 'blockUntil':blockUntil};
 	chrome.runtime.sendMessage(blockedSite);
 	window.close();
 }
