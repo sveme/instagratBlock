96564ff4dfd213a0df505e3e411639d834ec9a46
Sven Mesecke <sven.mesecke@sveme.org>
2016-08-07 14:30:14 +0200

    Reversible blocking working fine, good code style

--- background/background.js
+++ background/background.js
@@ -1,13 +1,15 @@
+/* eslint no-undef: "error" */
 const blockMap = new Map();
-const patternList = [];
+const urlFilter = { urls: [] };
+
 /* helper functions */
-function createURLPattern(url){
+function createURLPattern(url) {
   /* create URL filter pattern that matches http and https */
-  const pattern = url.replace(/http[s]?/, "http.*");
+  const pattern = url.replace(/http[s]?:\/\//, '*://');
   return pattern;
 }
 
-function createURLRegExp(url){
+function createURLRegExp(url) {
   /* escape the provided url so that we can test it when the urlFilter
   matched (see here for the source:
   http://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript/3561711#3561711)
@@ -19,52 +21,59 @@ function createURLRegExp(url){
 
 /* handling new Blocks and webRequest event functions */
 function newBlock(block, reply) {
-  let regExpPattern = createURLRegExp(block.url); // for use in finding the time until its blocked
-  //let filterPattern = createURLPattern(block.url); // for use in urlFilter object for webRequest event
-  blockMap.set(regExpPattern, {until: block.blockUntil, urlPattern: "filterPattern"}); //TODO
-  patternList.push(regExpPattern);
+  /* we use two different patterns describing the URL: the URL pattern is used for
+  the webRequest event listeners and uses the scheme defined for webextensions; the
+  RegExp pattern is used to figure out which URL pattern was actually triggered
+  */
+  const regExpPattern = createURLRegExp(block.url); // for use in finding the time until its blocked
+  const filterPattern = createURLPattern(block.url); // for use in urlFilter object
+  blockMap.set(regExpPattern, { until: block.blockUntil, urlPattern: filterPattern }); // TODO
+  urlFilter.urls.push(filterPattern);
+  chrome.webRequest.onBeforeRequest.addListener(checkBlock, urlFilter, ['blocking']);
   console.log(blockMap);
-  reply({message:'url blocked'});
+  reply({ message: 'url blocked' });
 }
 
 function checkBlock(details) {
-  console.log(details);
-  if (patternList.length == 0){
-    return {cancel: false};
+  if (blockMap.size === 0) {
+    return { cancel: false };
   }
   // find the matching pattern
-  let matchedPattern = patternList.filter(function (pattern){
-      return pattern.test(details.url);
-    });
-  console.log(matchedPattern.length);
+  let matchedUrlPattern;
+  let matchedRegExpPattern;
+  blockMap.forEach((val, key) => {
+    const rkey = new RegExp(key); // apparently some bug in firefox? key is a string, not a regExp!
+    if (rkey.test(details.url)) {
+      matchedRegExpPattern = rkey;
+      matchedUrlPattern = val;
+    }
+  }, blockMap);
 
+  console.log(matchedUrlPattern);
   let block = false;
   const now = Date.now();
-  const blockUntil = blockMap.get(matchedPattern[0]).until;
+  const blockUntil = matchedUrlPattern.until;
   if (now < blockUntil) {
     block = true;
   }
   else {
     block = false;
-    blockMap.delete(matchedPattern[0]); // remove block when block time is superseded
-    const pos = patternList.indexOf(matchedPattern[0]); //get the associated url pattern
-    patternList.splice(pos, 1);
+    const pos = urlFilter.urls.indexOf(matchedUrlPattern.urlPattern);
+    blockMap.delete(matchedRegExpPattern); // remove block when block time is superseded
+    urlFilter.urls.splice(pos, 1);
+    chrome.webRequest.onBeforeRequest.removeListener(checkBlock);
+    chrome.webRequest.onBeforeRequest.addListener(checkBlock, urlFilter, ['blocking']);
   }
   console.log(blockMap);
-  console.log(matchedPattern);
-  console.log(patternList);
   if (block) {
-    console.log("blocked!")
-    const redirectSiteUrl = chrome.extension.getURL("resources/blocked.html");
-    return { redirectUrl: redirectSiteUrl };
+    console.log("blocked!");
+    // const redirectSiteUrl = chrome.extension.getURL("resources/blocked.html");
+    return { cancel: true };
   }
   else {
-    console.log("Not blocked!")
-    return {cancel: false};
+    console.log("Not blocked!");
+    return { cancel: false };
   }
 }
-
 /* event handling: webRequest and message communication */
 chrome.runtime.onMessage.addListener(newBlock);
-const urlFilter = { urls: ["<all_urls>"] };
-chrome.webRequest.onBeforeRequest.addListener(checkBlock, urlFilter, ['blocking']);
