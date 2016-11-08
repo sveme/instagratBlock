ff69ef053528b1e939b58d7d073ea0fb71cae801
Sven Mesecke <me@sveme.org>
2016-11-08 09:56:27 +0100

    Animated new blocking symbol

--- background/blockIndicator.js
+++ background/blockIndicator.js
@@ -1,9 +1,19 @@
-let blockdiv = document.createElement('div');
+if (!blockdiv) {
+  let blockdiv = document.createElement('div');
+}
 blockdiv.className = 'blockIndicator';
+
 let img = document.createElement('img');
-img.setAttribute("src", chrome.extension.getURL("/icons/instagratBlock-32.png"));
-//img.setAttribute("src", "/icons/instagratBlock-32.png");
-img.setAttribute("style", "width: 32px height: 32px");
-img.setAttribute("alt", "Could not be loaded");
+img.setAttribute("src", chrome.extension.getURL("/icons/blockSymbol.png"));
+//img.setAttribute("style", "width: 32px height: 32px");
+img.setAttribute("alt", "Blocked!");
+
 blockdiv.appendChild(img);
 document.body.appendChild(blockdiv);
+
+function removeBlockdiv() {
+  let el = document.getElementsByClassName('blockIndicator')[0];
+  el.remove();
+}
+
+window.setTimeout(removeBlockdiv, 2500);
